4940a7b045eb637b718c2496b52ce975c8e92909
CatKevin <catmi9944@gmail.com>
2025-11-24 03:47:55 +0800

    upd(contracts, cli, web): deploy contracts and update package id

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const WIT_PACKAGE_ID = '0xd4c5cfc6e267bb99c0e8664a7ccd07aa89120316b443795528efd2ddd5f94d52';
+export const WIT_PACKAGE_ID = '0x48b1216ac1bf2d0926f10712694b0a0ed50072319a84f789f924a31d5af7ac7b';
 export const WIT_MODULE_NAME = 'repository';
